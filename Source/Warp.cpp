ea9d6c77958f6c3fda0ee06b6ed4d5ec6b98a416
エグチダイキ <74522674+daiki2001@users.noreply.github.com>
2021-10-27 17:55:11 +0900

    ワープの演出

--- Source/Warp.cpp
+++ Source/Warp.cpp
@@ -1,4 +1,16 @@
 #include "./Header/Warp.h"
+#include <stdlib.h>
+
+size_t Warp::warpCount = 0;
+
+const float Warp::emitRadius = 50.0f;
+const float Warp::maxTime = 100.0f;
+
+Particle Warp::particle[Warp::maxParticleNum * Warp::maxWarp] = {};
+int Warp::particlePolygon = 0;
+DirectX::XMFLOAT3 Warp::emitPos[Warp::maxParticleNum * Warp::maxWarp] = {};
+float Warp::time = 0.0f;
+float Warp::timeRate[Warp::maxParticleNum * Warp::maxWarp] = {};
 
 Warp::Warp() :
 	draw(nullptr),
@@ -19,12 +31,17 @@ void Warp::Init(DrawPolygon* draw)
 
 		this->draw = draw;
 		warpModel = this->draw->CreateOBJModel("./Resources/warp/wa-pu.obj", "./Resources/warp/");
+		particlePolygon = this->draw->CreateTriangle(
+			XMFLOAT3(-1.0f, -1.0f, 0.0f), XMFLOAT2(0.0f, 1.0f),
+			XMFLOAT3(+0.0f, +1.0f, 0.0f), XMFLOAT2(0.5f, 0.0f),
+			XMFLOAT3(+1.0f, -1.0f, 0.0f), XMFLOAT2(1.0f, 1.0f)
+		);
 	}
 }
 
 void Warp::WarpInit(const int map[MAP_HEIGHT][MAP_WIDTH], const int& stageNo)
 {
-	warpPos.clear();
+	warpCount = 0;
 
 	for (int y = 0; y < MAP_HEIGHT; y++)
 	{
@@ -32,7 +49,8 @@ void Warp::WarpInit(const int map[MAP_HEIGHT][MAP_WIDTH], const int& stageNo)
 		{
 			if (map[y][x] == ObjectStatus::WARP)
 			{
-				warpPos.push_back(XMFLOAT3((float)(x), (float)(y), 0.0f));
+				warpPos[warpCount] = XMFLOAT2((float)(x), (float)(y));
+				warpCount++;
 			}
 		}
 	}
@@ -50,14 +68,14 @@ void Warp::WarpInit(const int map[MAP_HEIGHT][MAP_WIDTH], const int& stageNo)
 		break;
 	}
 
-	if (LoadCSV1D(warpData, warpPos.size() * 2, filePath, -2) != 0)
+	if (LoadCSV1D(warpData, maxWarp * 2, filePath, -2) != 0)
 	{
 		return;
 	}
 
-	for (size_t i = 0; i < warpPos.size() * 2; i++)
+	for (size_t i = 0; i < warpCount * 2; i++)
 	{
-		if (i < warpPos.size())
+		if (i < warpCount)
 		{
 			if (warpData[i])
 			{
@@ -70,16 +88,26 @@ void Warp::WarpInit(const int map[MAP_HEIGHT][MAP_WIDTH], const int& stageNo)
 		}
 		else
 		{
-			warpExit[i - warpPos.size()] = warpData[i];
+			warpExit[i - warpCount] = warpData[i];
 		}
 	}
+
+	for (size_t i = 0; i < maxParticleNum * warpCount; i++)
+	{
+		particle[i].rotaMat = XMMatrixIdentity();
+		particle[i].scale = scale_xyz(1.0f);
+		particle[i].DelFlag = true;
+		timeRate[i] = 0.0f;
+	}
+
+	time = 0;
 }
 
-const Warp::XMFLOAT3& Warp::WarpPos(const int& warpNo)
+const Warp::XMFLOAT2& Warp::WarpPos(const int& warpNo)
 {
-	if (warpNo < 0 || warpNo >= warpPos.size())
+	if (warpNo < 0 || warpNo >= warpCount)
 	{
-		return warpPos[warpNo];
+		return XMFLOAT2(-1.0f, -1.0f);
 	}
 
 	if (warpFlag[warpNo])
@@ -88,11 +116,103 @@ const Warp::XMFLOAT3& Warp::WarpPos(const int& warpNo)
 	}
 	else
 	{
-		return warpPos[warpNo];
+		return XMFLOAT2(-1.0f, -1.0f);
 	}
 }
 
-void Warp::Draw(const XMFLOAT3& offset)
+void Warp::ParticleUpdate(const int& warpNo)
+{
+	if (warpNo < 0 || warpNo >= warpCount)
+	{
+		return;
+	}
+
+	using namespace DirectX;
+
+	static int angle1 = 0;
+	angle1 = rand() % 360;
+
+	// ベジェ曲線用の制御点
+	static Vector3 a = {}, b = {}, c = {};
+
+	time += 1.0f;
+
+	for (size_t i = 0; i < maxParticleNum; i++)
+	{
+		if (particle[i + (warpNo % warpCount) * maxParticleNum].DelFlag == true)
+		{
+			static int angle2 = 0;
+			angle2 = rand() % 180 - 90;
+
+			emitPos[i + (warpNo % warpCount) * maxParticleNum].y = emitRadius * sinf(2 * XM_PI / angle1 * 360) / 2.0f;
+			emitPos[i + (warpNo % warpCount) * maxParticleNum].z = emitRadius * cosf(2 * XM_PI / angle1 * 360) / 2.0f;
+			particle[i + (warpNo % warpCount) * maxParticleNum].rotaMat = XMMatrixIdentity();
+			particle[i + (warpNo % warpCount) * maxParticleNum].rotaMat *= XMMatrixRotationZ(XMConvertToRadians((float)angle2));
+			particle[i + (warpNo % warpCount) * maxParticleNum].rotaMat *= XMMatrixRotationX(XMConvertToRadians((float)angle2));
+			particle[i + (warpNo % warpCount) * maxParticleNum].DelFlag = false;
+			particle[i + (warpNo % warpCount) * maxParticleNum].time = time; //スタートタイム
+			timeRate[i] = 0.0f;
+
+			break;
+		}
+		else
+		{
+			timeRate[i + (warpNo % warpCount) * maxParticleNum] = (time - particle[i + (warpNo % warpCount) * maxParticleNum].time) / maxTime;
+
+			if (timeRate[i + (warpNo % warpCount) * maxParticleNum] >= 1.0f)
+			{
+				particle[i + (warpNo % warpCount) * maxParticleNum].DelFlag = true;
+				continue;
+			}
+
+			a = lerp(
+				{ -emitRadius, emitPos[i + (warpNo % warpCount) * maxParticleNum].y, emitPos[i + (warpNo % warpCount) * maxParticleNum].z },
+				{ -emitRadius, 0, 0 },
+				timeRate[i + (warpNo % warpCount) * maxParticleNum]
+			);
+			b = lerp({ -emitRadius / 2.0f, 0, 0 }, { emitRadius / 2.0f, 0, 0 }, timeRate[i + (warpNo % warpCount) * maxParticleNum]);
+			c = lerp(
+				{ emitRadius, 0, 0 },
+				{ emitRadius, emitPos[i + (warpNo % warpCount) * maxParticleNum].y, emitPos[i + (warpNo % warpCount) * maxParticleNum].z },
+				timeRate[i + (warpNo % warpCount) * maxParticleNum]
+			);
+
+			if (warpFlag[warpNo])
+			{
+				particle[i + (warpNo % warpCount) * maxParticleNum].pos =
+					lerp(a, b, timeRate[i + (warpNo % warpCount) * maxParticleNum]);
+			}
+			else
+			{
+				particle[i + (warpNo % warpCount) * maxParticleNum].pos =
+					lerp(b, c, timeRate[i + (warpNo % warpCount) * maxParticleNum]);
+			}
+		}
+	}
+}
+
+void Warp::Draw(const int& warpNo, const XMFLOAT3& offset)
 {
 	draw->DrawOBJ(warpModel, XMFLOAT3(offset.x, offset.y + 3.0f, offset.z), DirectX::XMMatrixIdentity(), scale_xyz(2.0f));
+
+	if (warpNo < 0 || warpNo >= warpCount)
+	{
+		return;
+	}
+
+	for (size_t i = 0; i < maxParticleNum; i++)
+	{
+		if (particle[i + (warpNo % warpCount) * maxParticleNum].DelFlag == true)
+		{
+			continue;
+		}
+
+		draw->Draw(
+			particlePolygon,
+			particle[i + (warpNo % warpCount) * maxParticleNum].pos + Vector3(offset.x, offset.y + 3.0f, offset.z),
+			particle[i + (warpNo % warpCount) * maxParticleNum].rotaMat,
+			particle[i + (warpNo % warpCount) * maxParticleNum].scale,
+			particle[i + (warpNo % warpCount) * maxParticleNum].color
+		);
+	}
 }
--- main.cpp
+++ main.cpp
@@ -198,7 +198,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 				upVec, MAIN_CAMERA
 			);
 
-			stageNo = 0;
 			directing.stageSelectInit();
 		case GameStatus::Select:
 			if (Input::IsKey(DIK_LEFT) && Input::IsKey(DIK_RIGHT))
@@ -214,8 +213,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 					if (directing.stageSelectFlag == true)
 					{
 						stageNo--;
-						directing.stageSelectEasingStart(XMFLOAT3(-window_width * (stageNo + 1), 0, 0),
-							XMFLOAT3(-window_width * stageNo, 0, 0), 40);
+						directing.stageSelectEasingStart(XMFLOAT3((float)(-window_width * (stageNo + 1)), 0, 0),
+							XMFLOAT3((float)(-window_width * stageNo), 0, 0), 40.0f);
 
 						if (stageNo < 0)
 						{
@@ -232,8 +231,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 				{
 					if (directing.stageSelectFlag == true)
 					{
-						directing.stageSelectEasingStart(XMFLOAT3(-window_width * stageNo, 0, 0),
-							XMFLOAT3(-window_width * (stageNo + 1), 0, 0), 40);
+						directing.stageSelectEasingStart(XMFLOAT3((float)(-window_width * stageNo), 0, 0),
+							XMFLOAT3((float)(-window_width * (stageNo + 1)), 0, 0), 40.0f);
 
 						stageNo++;
 						if (stageNo == 5)
@@ -373,7 +372,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 			{
 				if (isClear == true || isGameover == true)
 				{
-
 					directing.StartSceneChange();
 				}
 				else
@@ -634,7 +632,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 						{
 							auto temp = warp.WarpPos(warpCount);
 
-							if (temp.x != (float)x || temp.y != (float)y)
+							if ((temp.x != -1.0f) || (temp.y != -1.0f))
 							{
 								XMFLOAT3 warpPosition = {
 									temp.x * blockSize.x + mapOffset.x,
@@ -642,10 +640,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 									mapOffset.z
 								};
 								player.pos = warpPosition;
-								player.cameraPosX = player.pos.x;
 								player.collision.Initilize(player.pos, player.rotaMat, 5, 5, 5);
 							}
 						}
+						warp.ParticleUpdate(warpCount);
 						warpCount++;
 					}
 					break;
@@ -769,7 +767,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 
 		if (Input::IsKey(DIK_LEFT))
 		{
-			isClear = true;
 		}
 		if (Input::IsKey(DIK_RIGHT))
 		{
@@ -810,6 +807,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 			scrollX += window_width * 2;
 		}
 
+		warpCount = 0;
+
 		switch (oldGameStatus)
 		{
 		case GameStatus::TitleInit:
@@ -832,8 +831,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 				directing.SelectTitle();
 			}
 			//ステージセレクト描画
-
-
 			directing.StageSelectDraw(stageScore.score, stageScore.medal, window_width, window_height);
 		}
 		break;
@@ -891,7 +888,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 						draw.DrawOBJ(
 							itemModel,
 							mapPosition,
-							XMMatrixRotationX(XMConvertToRadians(0)),
+							XMMatrixIdentity(),
 							XMFLOAT3(5.0f, 5.0f, 5.0f),
 							XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f)
 						);
@@ -900,7 +897,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 						draw.DrawOBJ(
 							ringModel,
 							mapPosition,
-							XMMatrixRotationY(XMConvertToRadians(0)),
+							XMMatrixIdentity(),
 							XMFLOAT3(10.0f, 10.0f, 10.0f),
 							changeColor[ringColor[ringCount % (sizeof(ringColor) / sizeof(ringColor[0]))]]
 						);
@@ -990,7 +987,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 						);
 						break;
 					case ObjectStatus::WARP:
-						warp.Draw(mapPosition);
+						warp.Draw(warpCount, mapPosition);
+						warpCount++;
 						break;
 					case ObjectStatus::COLLECTION:
 						draw.DrawOBJ(
@@ -1103,7 +1101,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 			}
 #if _DEBUG
 			draw.DrawString(0, 0, 4.0f, XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), "laps:%d", laps);
-			draw.DrawString(0, 128, 4.0f, XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), "stage:%d", stageNo + 1);
+			draw.DrawString(0, 64, 4.0f, XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), "stage:%d", stageNo + 1);
 #endif // _DEBUG
 			break;
 		default:
